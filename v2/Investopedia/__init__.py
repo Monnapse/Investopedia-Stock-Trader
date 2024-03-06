53eb13a1afca4b9d04c6a03cb7140f2df9575577
Monnapse <masonshuber@gmail.com>
2024-03-06 13:44:32 -0700

    e

--- v2/Investopedia/__init__.py
+++ v2/Investopedia/__init__.py
@@ -6,6 +6,7 @@
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
 from enum import Enum
 
@@ -53,7 +54,7 @@ class Account:
     def new_page(self, url: str):
         driver.get(self.base_url + url)
 
-        time.sleep(5) # Wait for page to be loaded
+        time.sleep(3) # Wait for page to be loaded
 
     def get_account_overview(self):
         """
@@ -75,18 +76,61 @@ class Account:
 
         self.new_page("/simulator/trade/stocks")
 
+        # SYMBOL
         symbol_input = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Look up Symbol/Company Name"]')
         symbol_input.send_keys(symbol)
-
         time.sleep(2)
-
         symbols_list = driver.find_element(By.CLASS_NAME, "v-select-list")
-        first_stock = symbols_list.find_element(By.XPATH, "//div")
-        
+        first_stock = symbols_list.find_element(By.XPATH, "div[1]")
+
         #first_stock = driver.find_element(By.ID, "list-item-308-0")
         if not first_stock:
             print(f"Could not find symbol {symbol}")
             return None
         
-        return first_stock
+        first_stock.click()
 
+        # ACTION
+        action_input = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[1]/div/div[1]/div/div/div[1]').click()
+        if action == 1:
+            # Buying
+            driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/div/div[1]').click()
+        elif action == 2:
+            # Selling
+            driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/div/div[2]').click()
+
+        # QUANTITY
+        quantity_input = driver.find_element(By.CSS_SELECTOR, 'input[role="select-quantity"]')
+        quantity_input.send_keys(quantity)
+
+        # Order Type
+        order_type_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[1]/div/div/div[1]/div[1]/div[1]').click()
+        if order_type == 1:
+            driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[1]").click()
+        elif order_type == 2:
+            driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[2]").click()
+        elif order_type == 3:
+            driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[3]").click()
+
+        # Duration
+        #duration_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[2]/div/div/div[1]/div[1]').click()
+        #if duration == 1:
+        #    driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[6]/div/div[1]").click()
+        #elif duration == 2:
+        #    driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[6]/div/div[2]").click()
+            
+        # Price
+        #if order_type != 2:
+        #    price_input = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/main/div/div[2]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[2]/div/div/div/div[1]/div/input")
+        #    if price_input:
+        #        price_input.send_keys(price)
+
+        # Preview Order
+        time.sleep(5)
+        driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[3]/div/div[2]/button').click()
+        time.sleep(2)
+
+        # Submit Order
+        driver.find_element(By.LINK_TEXT, '//*[@id="app"]/div[3]/div/div/div[3]/div/div/div[2]/button').click()
+
+        print(f"Successfully traded {quantity} {symbol} stocks")
--- v2/main.py
+++ v2/main.py
@@ -3,5 +3,5 @@ import Investopedia
 client = Investopedia.Account("astromonkey", 'Masonman0123!')
 
 #print(client.get_account_overview())
-print(client.trade("dell", Investopedia.Action.buy, 1, Investopedia.OrderType.market))
+print(client.trade("dell", Investopedia.Action.buy, 3, Investopedia.OrderType.market))
 #print(client)
