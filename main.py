6bb08a5f4a0cb0b25a9fa02430f6b5cbc151faf4
Monnapse <masonshuber@gmail.com>
2024-03-06 21:35:33 -0700

    e

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """
-    Stock Links
-    NYSE - http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download
-    NASDAQ - http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download
-    AMEX - http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download
+    Stock Link - http://www.nasdaq.com/screening/
 """
 import requests
+import csv
+import pandas
 
 #import Investopedia
 
@@ -13,4 +12,14 @@ import requests
 #print(client.change_game_session("Investopedia Trading Game"))
 #print(client.get_account_overview())
 #print(client.trade("dell", Investopedia.Action.buy, 5))
-print(requests.get("http://www.nasdaq.com/api/screener/stocks?tableonly=true&limit=25&offset=0&download=true").text)
+
+#with open('stocks.csv', mode ='r')as file:
+#  csvFile = csv.DictReader(file)
+#  for row in csvFile:
+#        print(row["Symbol"])
+#        
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+print(data)
