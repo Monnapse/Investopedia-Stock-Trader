9ec0c860fd95f19d148334535af9948c17524f7e
Monnapse <masonshuber@gmail.com>
2024-03-06 21:14:57 -0700

    update

--- Investopedia/__init__.py
+++ Investopedia/__init__.py
@@ -6,16 +6,22 @@
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from enum import Enum
 
+wait_time = 20
+
 # DRIVER
 options = Options() 
-#options.headless = True
-#options.add_argument("--headless=new")
+
+options.headless = True
+options.add_argument("--headless=new")
+
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options) 
+web_driver_waiter = WebDriverWait(driver, wait_time)
 
 class Duration(Enum):
     day_only = 1
@@ -42,34 +48,56 @@ class Account:
         self.new_page(self.portfolio_url)
 
         # Sign in
-        username_element = driver.find_element(By.ID, "username")
+        username_element = web_driver_waiter.until(EC.presence_of_element_located((By.ID, 'username')))#driver.find_element(By.ID, "username")
         username_element.send_keys(username)
 
-        password_element = driver.find_element(By.ID, "password")
+        password_element = web_driver_waiter.until(EC.presence_of_element_located((By.ID, 'password')))#driver.find_element(By.ID, "password")
         password_element.send_keys(password)
 
-        login_element = driver.find_element(By.ID, "login")
+        login_element = web_driver_waiter.until(EC.presence_of_element_located((By.ID, 'login')))#driver.find_element(By.ID, "login")
         login_element.click()
+
+        time.sleep(1)
+        try:
+            if driver.find_element(By.CLASS_NAME, "alert-error"):
+                # Restart
+                print("Could not login now restarting")
+                self.__init__(username, password)
+        except:
+            pass
 
     def new_page(self, url: str):
         driver.get(self.base_url + url)
 
-        time.sleep(3) # Wait for page to be loaded
+        time.sleep(1) # Wait for page to be loaded
 
     def get_account_overview(self):
         """
             Gets Accounts Value, Buying Power, and Cash
         """
+        
         self.new_page(self.portfolio_url)
         
         return {
-            "account_value": driver.find_element(By.CSS_SELECTOR, 'div[data-cy="account-value-text"]').text,
-            "buying_power": driver.find_element(By.CSS_SELECTOR, 'div[data-cy="buying-power-text"]').text,
-            "cash": driver.find_element(By.CSS_SELECTOR, 'div[data-cy="cash-text"]').text
+            "account_value": web_driver_waiter.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-cy="account-value-text"]'))).text, #driver.find_element(By.CSS_SELECTOR, 'div[data-cy="account-value-text"]').text,
+            "buying_power": web_driver_waiter.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-cy="buying-power-text"]'))).text,#driver.find_element(By.CSS_SELECTOR, 'div[data-cy="buying-power-text"]').text,
+            "cash": web_driver_waiter.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[data-cy="cash-text"]'))).text#driver.find_element(By.CSS_SELECTOR, 'div[data-cy="cash-text"]').text
         }
     
+    def change_game_session(self, session_name: str):
+        #data-cy="portfolio-select"
+        #self.new_page("/simulator/trade/stocks")
+        web_driver_waiter.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-cy="portfolio-select"]'))).click()
+        #/html/body/div[1]/div/div/div[2]/main/div/div[1]/div/div[1]/div[2]/div/div/div/div
+        list = web_driver_waiter.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div/main/div/div[1]/div/div[5]/div/div/div/div')))
+        try:
+            session = list.find_element(By.XPATH, f"//*[contains(text(), '{session_name}')]")
+            session.click()
+        except:
+            pass
+
     #def get_stocks
-    def trade(self, symbol: str, action, quantity: int, order_type, price=0, duration=0):
+    def trade(self, symbol: str, action: Action, quantity: int, order_type: OrderType=OrderType.market, price=0, duration:Duration=Duration.day_only):
         """
             Buys stock
         """
@@ -77,10 +105,10 @@ class Account:
         self.new_page("/simulator/trade/stocks")
 
         # SYMBOL
-        symbol_input = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Look up Symbol/Company Name"]')
+        symbol_input = web_driver_waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Look up Symbol/Company Name"]')))#driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Look up Symbol/Company Name"]')
         symbol_input.send_keys(symbol)
-        time.sleep(2)
-        symbols_list = driver.find_element(By.CLASS_NAME, "v-select-list")
+       # time.sleep(2)
+        symbols_list = web_driver_waiter.until(EC.visibility_of_element_located((By.CLASS_NAME, 'v-select-list')))#driver.find_element(By.CLASS_NAME, "v-select-list")
         first_stock = symbols_list.find_element(By.XPATH, "div[1]")
 
         #first_stock = driver.find_element(By.ID, "list-item-308-0")
@@ -91,46 +119,68 @@ class Account:
         first_stock.click()
 
         # ACTION
-        action_input = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[1]/div/div[1]/div/div/div[1]').click()
-        if action == 1:
+        #action_input = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[1]/div/div[1]/div/div/div[1]').click()
+        web_driver_waiter.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[1]/div/div[1]/div/div/div[1]'))).click()
+        action_list = web_driver_waiter.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[1]/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div')))
+        if action.value == 1:
             # Buying
-            driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/div/div[1]').click()
-        elif action == 2:
+            #driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/div/div[1]').click()
+            action_list.find_element(By.XPATH, "div[1]").click()
+        elif action.value == 2:
             # Selling
-            driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/div/div[2]').click()
+            #driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/div/div[2]').click()
+            print("selling")
+            action_list.find_element(By.XPATH, "div[2]").click()
 
         # QUANTITY
-        quantity_input = driver.find_element(By.CSS_SELECTOR, 'input[role="select-quantity"]')
+        quantity_input = web_driver_waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[role="select-quantity"]')))#driver.find_element(By.CSS_SELECTOR, 'input[role="select-quantity"]')
         quantity_input.send_keys(quantity)
 
         # Order Type
-        order_type_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[1]/div/div/div[1]/div[1]/div[1]').click()
-        if order_type == 1:
-            driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[1]").click()
-        elif order_type == 2:
-            driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[2]").click()
-        elif order_type == 3:
-            driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[3]").click()
+        #order_type_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[1]/div/div/div[1]/div[1]/div[1]').click()
+        #web_driver_waiter.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[1]/div/div/div[1]/div[1]/div[1]'))).click()
+        #if order_type.value == 1:
+        #    #driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[1]").click()
+        #    web_driver_waiter.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[5]/div/div[1]'))).click()
+        #elif order_type.value == 2:
+        #    #driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[2]").click()
+        #    web_driver_waiter.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[5]/div/div[2]'))).click()
+        #elif order_type.value == 3:
+        #    #driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/div[3]").click()
+        #    web_driver_waiter.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[5]/div/div[3]'))).click()
 
         # Duration
         #duration_button = driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[2]/div/div/div[1]/div[1]').click()
-        #if duration == 1:
+        #if duration.value == 1:
         #    driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[6]/div/div[1]").click()
-        #elif duration == 2:
+        #elif duration.value == 2:
         #    driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[6]/div/div[2]").click()
             
         # Price
-        #if order_type != 2:
+        #if order_type.value != 2:
         #    price_input = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/main/div/div[2]/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[2]/div/div/div/div[1]/div/input")
         #    if price_input:
         #        price_input.send_keys(price)
 
         # Preview Order
-        time.sleep(5)
-        driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[3]/div/div[2]/button').click()
-        time.sleep(2)
+        #time.sleep(5)
+        # driver.find_element(By.XPATH, '//*[@id="app"]/div/main/div/div[3]/div[2]/div[2]/div[1]/div[2]/form/div[3]/div/div[2]/button').click()
+        #
+        # time.sleep(2)
+        preview_button = web_driver_waiter.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-cy="preview-button"]')))
+        #preview_button.click()
+        driver.execute_script("arguments[0].click()", preview_button)
+
+        time.sleep(1)
+        try:
+            if driver.find_element(By.CSS_SELECTOR, 'li[data-cy="stock-trade-error"]'):
+                return None, driver.find_element(By.CSS_SELECTOR, 'li[data-cy="stock-trade-error"]').text
+        except:
+            pass
 
         # Submit Order
-        driver.find_element(By.LINK_TEXT, '//*[@id="app"]/div[3]/div/div/div[3]/div/div/div[2]/button').click()
+        web_driver_waiter.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-cy="submit-order-button"]'))).click()
+
+        #driver.find_element(By.LINK_TEXT, '/html/body/div[1]/div/div/div[3]/div/div/div[3]/div/div/div[2]').click()
 
         print(f"Successfully traded {quantity} {symbol} stocks")
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import Investopedia
+"""
+    Stock Links
+    NYSE - http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download
+    NASDAQ - http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download
+    AMEX - http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=amex&render=download
+"""
+import requests
 
-client = Investopedia.Account("astromonkey", 'Masonman0123!')
+#import Investopedia
 
+#client = Investopedia.Account("astromonkey", 'Masonman0123!')
+
+#print(client.change_game_session("Investopedia Trading Game"))
 #print(client.get_account_overview())
-print(client.trade("dell", Investopedia.Action.buy, 3, Investopedia.OrderType.market))
-#print(client)
+#print(client.trade("dell", Investopedia.Action.buy, 5))
+print(requests.get("http://www.nasdaq.com/api/screener/stocks?tableonly=true&limit=25&offset=0&download=true").text)
